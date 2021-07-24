0cb134dcf502be1b8fd1729f5aa4e38eb6c2fdd5
Dong-hee Na <donghee.na@python.org>
2021-07-24 21:33:39 +0900

    discord-bot: Setting up initial project structure

---
+++ bot/app.py
@@ -0,0 +1,19 @@
+import os
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!")
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}({bot.user.id})")
+
+
+@bot.command(name="ping")
+async def ping(ctx):
+    await ctx.send("pong")
+
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
