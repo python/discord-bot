9ca04a07ee38c030a0fe3577e9518776b9d402e0
Dong-hee Na <donghee.na@python.org>
2023-03-16 12:32:29 +0900

    tldr: Implement prototype of PEP summerize bot.

--- bot/app.py
+++ bot/app.py
@@ -1,12 +1,21 @@
 import os
+
+import httpx
+import tiktoken
 from discord.ext import commands
 
+from util_openai import get_pep_text, text_to_chunks, send_partial_text, summarize
+
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
 bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
+async_http_client = httpx.AsyncClient()
 
 
 @bot.event
 async def on_ready():
+    # TODO: Need to use logger
     print(f"Logged in as {bot.user.name}({bot.user.id})")
 
 
@@ -15,5 +24,26 @@ async def ping(ctx):
     await ctx.send("pong")
 
 
+@bot.command(name="tldr")
+async def tldr(ctx, target: str, number: int):
+    if target.lower() != "pep":
+        await ctx.send("Only supports for PEP doucments right now.")
+
+    try:
+        target_pep = f"pep-{number:04d}"
+        pep_text = await get_pep_text(async_http_client, target_pep)
+        encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+        responses = []
+        for chunk in text_to_chunks(encoding, pep_text):
+            text = encoding.decode(chunk)
+            res = await send_partial_text(text, target_pep)
+            responses.append(res)
+
+        summary = await summarize(target_pep, responses)
+        await ctx.send(summary)
+    except Exception as e:
+        await ctx.send(f"Error: {e}")
+
+
 if __name__ == "__main__":
-    bot.run(TOKEN)
+    bot.run(DISCORD_TOKEN)
---
+++ bot/util_openai.py
@@ -0,0 +1,67 @@
+import os
+
+import openai_async
+
+OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
+
+
+def text_to_chunks(encoding, text: str, chunk_size: int = 2000, overlap: int = 100):
+    tokens = encoding.encode(text)
+    num_tokens = len(tokens)
+    for i in range(0, num_tokens, chunk_size - overlap):
+        chunk = tokens[i : i + chunk_size]
+        yield chunk
+
+
+async def get_pep_text(client, target_pep: str):
+    target = f"https://raw.githubusercontent.com/python/peps/main/{target_pep}.rst"
+    res = await client.get(target)
+    if res.status_code == 404:
+        # fallback
+        target = f"https://raw.githubusercontent.com/python/peps/main/{target_pep}.txt"
+        res = await client.get(target)
+    return res.text
+
+
+async def send_partial_text(text: str, target_pep: str) -> str:
+    prompt_request = f"Summarize this documentation: {text})"
+    messages = [
+        {"role": "system", "content": f"This is text summarization for {target_pep}"}
+    ]
+    messages.append({"role": "user", "content": prompt_request})
+    response = await openai_async.chat_complete(
+        OPEN_AI_API_KEY,
+        timeout=60,
+        payload={
+            "model": "gpt-3.5-turbo",
+            "messages": messages,
+            "temperature": 0.3,
+            "max_tokens": 500,
+            "top_p": 1.0,
+            "frequency_penalty": 0.0,
+            "presence_penalty": 1,
+        },
+    )
+    res = response.json()
+    return res["choices"][0]["message"]["content"].strip()
+
+
+async def summarize(pep: str, texts: list[str]) -> str:
+    text = "\n".join(texts)
+    prompt_request = f"Please summarize the text with bullet points: {text}"
+    response = await openai_async.complete(
+        OPEN_AI_API_KEY,
+        timeout=60,
+        payload={
+            "model": "text-davinci-003",
+            "prompt": prompt_request,
+            "temperature": 0.3,
+            "max_tokens": 150,
+            "top_p": 1.0,
+            "frequency_penalty": 0.0,
+            "presence_penalty": 1,
+        },
+    )
+    res = response.json()
+    summary = res["choices"][0]["text"].strip()
+    return f"Summary for {pep}:\n{summary}"
