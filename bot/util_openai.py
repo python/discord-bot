4e81dba7e6f09d1157dfc3925b61f53af94c9518
Dong-hee Na <donghee.na@python.org>
2023-03-16 14:10:57 +0900

    little more improvement

--- bot/util_openai.py
+++ bot/util_openai.py
@@ -1,11 +1,14 @@
 import os
+from typing import Generator
 
 import openai_async
 
 OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
 
 
-def text_to_chunks(encoding, text: str, chunk_size: int = 2000, overlap: int = 100):
+def text_to_chunks(
+    encoding, text: str, chunk_size: int = 2000, overlap: int = 100
+) -> Generator[list[int], None, None]:
     tokens = encoding.encode(text)
     num_tokens = len(tokens)
     for i in range(0, num_tokens, chunk_size - overlap):
@@ -13,13 +16,15 @@ def text_to_chunks(encoding, text: str, chunk_size: int = 2000, overlap: int = 1
         yield chunk
 
 
-async def get_pep_text(client, target_pep: str):
+async def get_pep_text(client, target_pep: str) -> str:
     target = f"https://raw.githubusercontent.com/python/peps/main/{target_pep}.rst"
     res = await client.get(target)
     if res.status_code == 404:
         # fallback
         target = f"https://raw.githubusercontent.com/python/peps/main/{target_pep}.txt"
         res = await client.get(target)
+        if res.status_code == 404:
+            raise RuntimeError("Not found")
     return res.text
 
 
@@ -48,6 +53,7 @@ async def send_partial_text(text: str, target_pep: str) -> str:
 
 async def summarize(pep: str, texts: list[str]) -> str:
     text = "\n".join(texts)
+    link = f"https://peps.python.org/{pep}"
     prompt_request = f"Please summarize the text with bullet points: {text}"
     response = await openai_async.complete(
         OPEN_AI_API_KEY,
@@ -63,5 +69,6 @@ async def summarize(pep: str, texts: list[str]) -> str:
         },
     )
     res = response.json()
-    summary = res["choices"][0]["text"].strip()
-    return f"Summary for {pep}:\n{summary}"
+    content = res["choices"][0]["text"].strip()
+    summary = f"Summary for {pep}:\n{content}\n\nFor more information: {link}"
+    return summary
